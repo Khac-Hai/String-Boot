e4ca950bf8a57f939896629f85873715efe9310e
Nguyễn Khắc Hải <haikhac23122004@gmail.com>
2026-04-20 23:09:35 +0700

    Add files via upload

---
+++ ss11/src/test/java/re/edu/Ss11ApplicationTests.java
@@ -0,0 +1,13 @@
+package re.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
